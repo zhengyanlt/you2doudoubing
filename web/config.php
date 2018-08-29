d8bd1c654671520231ff8cc320375b74a64bd28e
doudoubinga <42226718+doudoubinga@users.noreply.github.com>
2018-08-29 23:09:04 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -1,9 +1,9 @@
 <?php
 define('ROOT_PART', Root_part());
-define('APIKEY', getenv("APIKEY"));
-define('GJ_CODE', getenv("GJ_CODE"));
-define('SITE_NAME',getenv("SITE_NAME"));
-define('TITLENAME', getenv("TITLENAME"));
-define('EN2DEKEY',getenv("EN2DEKEY"));
-define('EMAIL', getenv("EMAIL"));
+define('APIKEY', 'AIzaSyARHJJDFtwb5wcnURwwJA3FQRrM16PF27c');
+define('GJ_CODE', 'JP');
+define('SITE_NAME', '大爷常来玩儿啊');
+define('TITLENAME', 'DouDouTube');
+define('EN2DEKEY', 'lujianbupingyishenghou');
+define('EMAIL', 'xiaofufua@protonmail.com');
 ?>
