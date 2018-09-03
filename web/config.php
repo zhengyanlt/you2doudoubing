7224c2fd0c5b163a111edfb674dbb3959ca54898
zhengyanlt <42364069+zhengyanlt@users.noreply.github.com>
2018-09-03 19:55:01 -0400

    Update config.php

--- web/config.php
+++ web/config.php
@@ -1,9 +1,9 @@
 <?php
 define('ROOT_PART', Root_part());
 define('APIKEY', 'AIzaSyARHJJDFtwb5wcnURwwJA3FQRrM16PF27c');
-define('GJ_CODE', 'JP');
-define('SITE_NAME', '大爷常来玩儿啊');
-define('TITLENAME', 'DouDouTube');
+define('GJ_CODE', 'HK');
+define('SITE_NAME', '猪宝宝油管');
+define('TITLENAME', 'YanTube');
 define('EN2DEKEY', 'lujianbupingyishenghou');
 define('EMAIL', 'xiaofufua@protonmail.com');
 ?>
